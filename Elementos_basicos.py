6d4d9c1bcdfce2398c4eba3382d0e92ae61bc759
5antana <5antana@gmail.com>
2022-03-28 00:34:38 -0600

    Se agrega el Proyecto

---
+++ Elementos_basicos.py
@@ -0,0 +1,68 @@
+'''
+print es para mostrar o imprimir algo en la pantalla
+print("hola mundo\nminombre es Erick") la \n sirve para dar un salto de linea pero sin tener que poner otra vez print
+print(nombre de la variable) esto es para imprimir el valor de la variable
+
+para hacer una variable se hace esto: nombre_de_la_variable = valor de la variable
+ejemplo: numero = 10
+
+Los numeros int son  los numeros con un valor/numero entero
+Los numeros float son los reales, la diferencia de estos es que son los numeros enteros pero con punto decimal
+
+
+Cadenas:
+Las cadenas se pueden asignar en dos formas; con comillas dobles  o  con comillas simples
+Se pueden mezclar las comillas dobles con las comillas simples esto sirve para poder poner entre comillas alguna parte
+del texto, por ejemplo;
+cadena = 'estoy "estudiando"'
+Para que puedas colocarle un valor en texto a una variable se ocupan las comillas por ejemplo:
+cadena = "valor"
+
+Datos booleanos:
+Los tipos de datos booleanos simplemente hay dos; True o False
+Para poder poner tipos de datos booleanos la primera letra debe de ser mayuscula, ejemplo;
+nombre de la variable = True, o se puede colocar False
+
+Asignación de valores:
+Para hacer una suma con el valor de las variables se hace de este modo: ejemplo
+num1 = 10
+num2 =6.5
+Estas dos variables (num) son a las variables a las que le vamos a dar un valor
+nombre de la variable donde quieras que se guarde el resultado = num1+num2
+Tambien se puede divir en este caso se utiliza el simbolo /
+Tambien se pueden hacer varias operaciones en una sola linea, ejemplo;
+sumar = num1+num2 * 10/6
+Recuerda que python tambien respeta las reglas de presedencia de la matematica, pero si se quiere que
+primero se haga algo en concreto se debe de colocar en parentesis, ejemplo;
+sumar = (num1+num2) * 10/6
+Para que se muestre el resultado se hace de este modo, ejemplo;
+print("el texto que quieres que salga:",el valor) Para que se concatene con un valor solo se
+añade una (,) y despues el nombre donde esta almacenado el valor
+
+Python te deja ocupar tipado dinamico, que lo que hace que si tienes una variable y le colocaste diferentes valores,
+Python lo que hace es juntar los dos valores que le colocaste a la variable, ejemplo;
+
+valor = 10
+
+print(valor)
+
+valor = "Alejandro"
+
+print(valor)
+
+Por lo cual lo que te va enseñar print seria 10 Alejandro
+
+Comentarios:
+Los comentarios sirven a documentar el codigo para explicar que hace nuestro programa esto nos ayuda tanto a nosotros
+como tambien a otros programadores que lo lean
+Existen dos tipos de comentarios; los comentarios de una linea y los comentarios multilinea
+Los comentarios de una linea solo se debe agregar un numeral(#) al principio de la linea y ya lo que se escriba despues
+de este y esto hara que cuando se ejecute el programa lo va a obviar por lo cual no va a haber ningun problema
+Para hacer un comentario multilinea solo se debe de colocar tres comillas simples (') estas se colocan al principio y
+al final de lo que quieras comentar
+
+variables:
+(type) sirve para clasificar las variables
+
+
+'''
